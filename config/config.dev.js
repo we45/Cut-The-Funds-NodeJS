228b8c183b986da0062353b63ff48e361a54a023
Abhay Bhargav <abhay@we45.com>
2018-04-09 11:09:14 +0530

    Added "create_card" permission and fixed ListCard Bug

--- config/config.dev.js
+++ config/config.dev.js
@@ -9,7 +9,7 @@ module.exports = {
     'mongoUri': "mongodb://"+mongo_ip+":27017/cut_the_funds",
     'secret': "aec12a48-720c-4102-b6e1-d0d873627899",
     'salt': 'secretSalt',
-    'userPerms': ["view_project", "create_expense", "delete_expense", "view_expense", "modify_expense", "view_coupons"],
+    'userPerms': ["view_project", "create_expense", "delete_expense", "view_expense", "modify_expense", "view_coupons", "create_card"],
     'mgrPerms': ["create_project", "delete_project", "modify_project", "view_expense", "approve_expense", "view_project"],
     "uploadDir": upload_dir,
     "mysql_db": mysql_ip,
